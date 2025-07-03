e58a164e8a84768bdbab33957a1cb0a16c49736d
hemashree0809 <hemakothand07@gmail.com>
2025-07-03 15:15:26 +0530

    task 2 code is added

--- task2/calculator.py
+++ task2/calculator.py
@@ -1,33 +1,47 @@
-# Simple Calculator
 
-# Prompt user for input
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+    
+def show():
+    print("Choose the operation:")
+    print("1. Addition (+)")
+    print("2. Subtraction (-)")
+    print("3. Multiplication (*)")
+    print("4. Division (/)")
+    print("5. Exit")
+def main():
+    while True:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        show()
+        choice = input("Enter your choice (1/2/3/4): ")
 
-print("Choose the operation:")
-print("1. Addition (+)")
-print("2. Subtraction (-)")
-print("3. Multiplication (*)")
-print("4. Division (/)")
 
-choice = input("Enter your choice (1/2/3/4): ")
+        if choice == '1':
+            result = num1 + num2
+            print(f"The result of addition is: {result}")
+            print()
+        elif choice == '2':
+            result = num1 - num2
+            print(f"The result of subtraction is: {result}")
+            print()
+        elif choice == '3':
+            result = num1 * num2
+            print(f"The result of multiplication is: {result}")
+            print()
+        elif choice == '4':
+            if num2 != 0:
+                result = num1 / num2
+                print(f"The result of division is: {result}")
+                print()
+            else:
+                print("Error: Division by zero is not allowed.") 
+                print()
+        elif choice=='5':
+            print("Exiting!!")
+            break
+        else:
+            print("Invalid choice! Please select a valid operation.")
+            print()
 
-# Perform calculation based on choice
-if choice == '1':
-    result = num1 + num2
-    print(f"The result of addition is: {result}")
-elif choice == '2':
-    result = num1 - num2
-    print(f"The result of subtraction is: {result}")
-elif choice == '3':
-    result = num1 * num2
-    print(f"The result of multiplication is: {result}")
-elif choice == '4':
-    if num2 != 0:
-        result = num1 / num2
-        print(f"The result of division is: {result}")
-    else:
-        print("Error: Division by zero is not allowed.")
-else:
-    print("Invalid choice! Please select a valid operation.")
+if __name__ == "__main__":
+    main()
 
